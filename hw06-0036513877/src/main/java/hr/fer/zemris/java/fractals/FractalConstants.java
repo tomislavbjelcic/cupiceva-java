215fd3786d36dc618d550f732588005bbf983810
abjelcic <abjelcic@phy.hr>
2020-12-14 03:46:23 +0100

    testiranje

--- hw06-0036513877/src/main/java/hr/fer/zemris/java/fractals/FractalConstants.java
+++ hw06-0036513877/src/main/java/hr/fer/zemris/java/fractals/FractalConstants.java
@@ -6,4 +6,8 @@ public class FractalConstants {
 	public static final double CONVERGENCE_TRESHOLD = 0.001;
 	public static final double ROOT_TRESHOLD = 0.002;
 	
+	public static void main(String[] args) {
+		
+	}
+	
 }
