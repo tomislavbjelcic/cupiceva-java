bd73ff7d64be3ec801105f5720e3be26656f9e60
Tomislav Bjelčić <tomislav.bjelcic@fer.hr>
2020-12-14 09:15:55 +0100

    Promijenjena vrijednost max iteracija

--- hw06-0036513877/src/main/java/hr/fer/zemris/java/fractals/FractalConstants.java
+++ hw06-0036513877/src/main/java/hr/fer/zemris/java/fractals/FractalConstants.java
@@ -11,7 +11,7 @@ public class FractalConstants {
 	
 	private FractalConstants() {}
 	
-	public static final int MAX_ITERATIONS = 1 << 12;
+	public static final int MAX_ITERATIONS = 1 << 8;
 	public static final double CONVERGENCE_TRESHOLD = 0.001;
 	public static final double ROOT_TRESHOLD = 0.002;
 	
